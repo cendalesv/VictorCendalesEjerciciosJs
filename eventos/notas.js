5735fffa4a453e72c26f062a075696bea4331748
cendalesv <cendalescaceres@hotmail.com>
2024-07-17 20:42:30 -0500

    actualice una funcion

--- eventos/notas.js
+++ eventos/notas.js
@@ -1,4 +1,3 @@
-
 let notas = [
     {
         id: 1,
@@ -29,34 +28,32 @@ let notas = [
         tachada: false
     }
 ];
-let idGlobal = 4
-
+let idGlobal = 4;
 
 function pintarNotas(notasArray) {
-    let notasContainer = document.getElementById('notasContainer')
-    notasContainer.innerHTML = ''
+    let notasContainer = document.getElementById("notasContainer");
+    notasContainer.innerHTML = "";
 
     if (notasArray.length === 0) {
-        notasContainer.innerHTML = '<p> NO HAY NOTAS </p>'
-        return
+        notasContainer.innerHTML = "<p> NO HAY NOTAS </p>";
+        return;
     }
 
     notasArray.forEach(nota => {
-        let noteCard = document.createElement('div')
-        noteCard.classList.add('note-card')
+        let noteCard = document.createElement("div");
+        noteCard.classList.add("note-card");
         if (nota.tachada) {
-            noteCard.classList.add('tachada')
+            noteCard.classList.add("tachada");
         }
         noteCard.innerHTML = `
             <input onClick="marcarRealizada(${nota.id})" type="checkbox" ${nota.tachada ? "checked" : ""}>
             <h3>${nota.titulo}</h3>
             <p>${nota.texto}</p>
             <button onClick="borrarNota(${nota.id})">Borrar nota</button>
-        `
-        notasContainer.appendChild(noteCard)
-    })
+        `;
+        notasContainer.appendChild(noteCard);
+    });
 }
-
 
 function agregarNota(titulo, texto) {
     let nuevaNota = {
@@ -66,70 +63,64 @@ function agregarNota(titulo, texto) {
         realizada: false,
         tachada: false
     };
-    notas.push(nuevaNota)
-}
-
-
-function borrarNota(id) {
-    notas = notas.filter(nota => nota.id !== id)
-    aplicarFiltros();
-}
-
-
-function marcarRealizada(id) {
-    let nota = notas.find(nota => nota.id === id)
-    if (nota) {
-        nota.tachada = !nota.tachada
-    }
-    aplicarFiltros()
-}
-
-
-function filtrarPorRealizada(notasArray) {
-    let completado = document.getElementById('completado').checked
-    if (completado) {
-        return notasArray.filter(nota => nota.tachada)
-    }
-    return notasArray
-}
-
-
-function filtrarPorTexto(notasArray, texto) {
-    if (texto) {
-        return notasArray.filter(nota => nota.titulo.includes(texto) || nota.texto.includes(texto))
-    }
-    return notasArray
+    notas.push(nuevaNota);
 }
 
 function aplicarFiltros() {
-    let buscar = document.getElementById('buscar').value
-    let notasFiltradas = filtrarPorTexto(notas, buscar)
-    notasFiltradas = filtrarPorRealizada(notasFiltradas)
-    pintarNotas(notasFiltradas)
+    let buscar = document.getElementById("buscar").value;
+    let notasFiltradas = filtrarPorTexto(notas, buscar);
+    notasFiltradas = filtrarPorRealizada(notasFiltradas);
+    pintarNotas(notasFiltradas);
 }
 
+function borrarNota(id) {
+    notas = notas.filter(nota => nota.id !== id);
+    aplicarFiltros();
+}
 
-document.getElementById('guardarButton').addEventListener('click', () => {
-    let tituloInput = document.getElementById('tituloInput')
-    let textInput = document.getElementById('textInput')
-    let titulo = tituloInput.value
-    let texto = textInput.value
+function marcarRealizada(id) {
+    let nota = notas.find(nota => nota.id === id);
+    if (nota) {
+        nota.tachada = !nota.tachada;
+    }
+    aplicarFiltros();
+}
+
+function filtrarPorRealizada(notasArray) {
+    let completado = document.getElementById("completado").checked;
+    if (completado) {
+        return notasArray.filter(nota => nota.tachada);
+    }
+    return notasArray;
+}
+
+function filtrarPorTexto(notasArray, texto) {
+    if (texto) {
+        return notasArray.filter(nota => nota.titulo.includes(texto) || nota.texto.includes(texto));
+    }
+    return notasArray;
+}
+
+document.getElementById("guardarButton").addEventListener("click", () => {
+    let tituloInput = document.getElementById("tituloInput");
+    let textInput = document.getElementById("textInput");
+    let titulo = tituloInput.value;
+    let texto = textInput.value;
 
     if (titulo && texto) {
-        agregarNota(titulo, texto)
-        aplicarFiltros()
-        tituloInput.value = ''
-        textInput.value = ''
+        agregarNota(titulo, texto);
+        aplicarFiltros();
+        tituloInput.value = "";
+        textInput.value = "";
     }
-})
-
-document.getElementById('borrarButton').addEventListener('click', () => {
-    document.getElementById('tituloInput').value = ''
-    document.getElementById('textInput').value = ''
 });
 
-document.getElementById('buscar').addEventListener('input', aplicarFiltros)
-document.getElementById('completado').addEventListener('change', aplicarFiltros)
+document.getElementById("borrarButton").addEventListener("click", () => {
+    document.getElementById("tituloInput").value = "";
+    document.getElementById("textInput").value = "";
+});
 
+document.getElementById("buscar").addEventListener("input", aplicarFiltros);
+document.getElementById("completado").addEventListener("change", aplicarFiltros);
 
-aplicarFiltros()
+aplicarFiltros();
